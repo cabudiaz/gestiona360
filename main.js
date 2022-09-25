23c45484dfb07baebda76e95b1aa5be23273a103
cabudiaz <cabudiaz@gmail.com>
2022-09-25 15:39:08 -0500

    productos más vendidos JQuery

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ authGuard();
 function route() {
   $("#nav").load("nav.html");
   $("#modulos-kpi").load("modulos-kpi.html");
+  $("#productos-mas-vendidos").load("productos-mas-vendidos.html");
   $("#footer").load("footer.html");
 }
